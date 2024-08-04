dbe4e9e61a8b5b170cba56fc5bfa27ef6a7c7b51
Steve Jenson <stevej@gmail.com>
2024-08-03 20:06:26 -0700

    adding fnv-1a and lzc with basic formal verification

---
+++ src/fnv_1a_32.v
@@ -0,0 +1,34 @@
+`ifndef FNV_1A_32
+`define FNV_1A_32
+module fnv_1a_32(
+    input wire clk,
+    input wire reset,
+    input wire [31:0] in,
+    output wire [31:0] out
+    );
+
+parameter bit OFFSET_BASIS = 32'd2166136261;
+parameter bit FNV_PRIME = 32'd16777619;
+
+reg [31:0] hash;
+
+/**
+ * Original fnv-1a algorithm
+ * ----
+ * hash = offset_basis
+ * for each octet_of_data to be hashed
+ *   hash = hash xor octet_of_data
+ *   hash = hash * FNV_prime
+ * return hash
+ **/
+
+always @(posedge clk) begin
+    if (reset) hash = OFFSET_BASIS;
+
+    hash <= (hash ^ in) * FNV_PRIME;
+end
+
+assign out = hash;
+
+endmodule
+`endif
---
+++ src/lzc_formal.v
@@ -0,0 +1,21 @@
+`default_nettype none `timescale 1us / 100 ns
+
+`include "lzc.v"
+
+module lzc_formal (
+    input  logic [31:0] in,
+    output logic [31:0] out
+);
+
+  lzc lzc (
+      .in (in),
+      .out(out)
+  );
+
+`ifdef FORMAL
+  always_comb begin
+    cover (out == 32'h0);
+  end
+`endif
+
+endmodule
